37ee63b848a2d075099481911067cd8a98848a7f
Spytech102 <myalxsoftware@gmail.com>
2022-03-19 02:06:05 -0700

    largest done

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+:#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,14 +10,14 @@ int main(void)
 {
 	int a, b, c;
 	int largest;
-	
+
 	a = 972;
 	b = -98;
 	c = 0;
-	
+
 	largest = largest_number(a, b, c);
-	
+
 	printf("%d is the largest number\n", largest);
-	
+
 	return (0);
 }
